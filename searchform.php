caa03eb4cb58f88cfb600e850699a8e9df8a3c96
Siddharth Tikekar <siddharth.tikekar@rtcamp.com>
2019-12-05 18:37:33 +0530

    Add page comment in searchform.php

--- searchform.php
+++ searchform.php
@@ -1,3 +1,14 @@
+<?php
+/**
+ * This page contains HTML code for search form.
+ *
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package DESIGNfly
+ */
+?>
+
 <div id="searchform">
     <input type="text" class="search-input">
     <button><span class="dashicons dashicons-search"></span></button>
