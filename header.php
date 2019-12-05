9faaa78c80541aee8ecd334b5a2fcd88bb94e4c2
Siddharth Tikekar <siddharth.tikekar@rtcamp.com>
2019-12-05 18:50:12 +0530

    Add favicon through code

--- header.php
+++ header.php
@@ -16,14 +16,13 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<link rel="shortcut icon" href="<?= get_stylesheet_directory_uri() ?>/img/favicon.ico" />
 
 	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'designfly' ); ?></a>
-
 	<header id="masthead" class="site-header">
 		<div class="site-header-middle">
 			<div class="site-header-content">
