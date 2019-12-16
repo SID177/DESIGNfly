94ca850089154c07f4ce7ab10f7377acfd47749b
Siddharth Tikekar <siddharth.tikekar@rtcamp.com>
2019-12-16 13:48:19 +0530

    Add link to taxonomy in taxonomy-bar

--- template-parts/content-portfolio-tax.php
+++ template-parts/content-portfolio-tax.php
@@ -18,11 +18,13 @@
 			foreach ( $terms as $term ) {
 				?>
 				<div class="portfolio-taxonomy-block">
-					<img src="<?= get_template_directory_uri() . '/img/taxonomy-icons/' . esc_html( $term->slug ) . '.png' ?>">
-					<div class="portfolio-taxonomy-details">
-						<span class="portfolio-taxonomy-title"><?= esc_html( $term->name ) ?></span>
-						<p class="portfolio-taxonomy-description"><?= esc_html( $term->description ) ?></p>
-					</div>
+						<img src="<?= get_template_directory_uri() . '/img/taxonomy-icons/' . esc_html( $term->slug ) . '.png' ?>">
+						<div class="portfolio-taxonomy-details">
+							<a href="<?= get_term_link( $term, 'designfly_portfolio' ) ?>">
+								<span class="portfolio-taxonomy-title"><?= esc_html( $term->name ) ?></span>
+							</a>
+							<p class="portfolio-taxonomy-description"><?= esc_html( $term->description ) ?></p>
+						</div>
 				</div>
 				<?php
 			}
