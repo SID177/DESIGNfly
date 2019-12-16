a94730580eeb9167be79daf745b041951c1d5c49
Siddharth Tikekar <siddharth.tikekar@rtcamp.com>
2019-12-16 15:05:27 +0530

    Separate archive for taxonomy, create structure for blog page

--- home.php
+++ home.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The Blog Posts index page
+ * The Blog Posts index page.
  *
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
@@ -12,45 +12,54 @@ get_header();
 get_template_part( 'template-parts/content', 'portfolio-tax' );
 ?>
 
-	<div id="primary" class="content-area">
+	<div id="primary" class="content-area show-sidebar">
 		<main id="main" class="site-main">
 
-		<?php
-		if ( have_posts() ) :
+			<?php
+			if ( have_posts() ) :
 
-			if ( is_home() && ! is_front_page() ) :
 				?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
+				<div class="blog-content">
+					<div class="post-content">
+						<div class="main-title">
+							<span class="title"><?= esc_html__( "LET'S BLOG", 'designfly' ) ?></span>
+						</div>
+						<?php
+
+						/* Start the Loop */
+						while ( have_posts() ) :
+							the_post();
+
+							/*
+							* Include the Post-Type-specific template for the content.
+							* If you want to override this in a child theme, then include a file
+							* called content-___.php (where ___ is the Post Type name) and that will be used instead.
+							*/
+							get_template_part( 'template-parts/content', get_post_type() );
+
+						endwhile;
+
+						?>
+					</div>
+					<?php get_sidebar(); ?>
+				</div>
 				<?php
+
+				the_posts_pagination( array(
+					'screen_reader_text' => ' ',
+					'next_text'          => '<img src="' . get_template_directory_uri() . '/img/pagination-arrow.png' . '">',
+					'prev_text'          => '',
+				) );
+
+			else :
+
+				get_template_part( 'template-parts/content', 'none' );
+
 			endif;
-
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
-
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
+			?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
 <?php
-get_sidebar();
 get_footer();
---
+++ taxonomy-designfly_categories.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * The template for displaying designfly_portfolio archive page.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package DESIGNfly
+ */
+
+get_header();
+get_template_part( 'template-parts/content', 'portfolio-tax' );
+?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+
+			<div class="main-title">
+				<span><?= esc_html__( "D'SIGN IS THE SOUL", 'designfly' ) ?></span>
+				<div class="main-title-button">
+					<?php
+					$terms = get_terms( array( 'taxonomy' => 'designfly_categories', 'hide_empty' => false ) );
+					foreach ( $terms as $term ) {
+						?>
+						<a href="<?= get_term_link( $term, 'designfly_portfolio' ) ?>"><?= esc_html( $term->name ) ?></a>
+						<?php
+					}
+					?>
+				</div>
+			</div>
+
+			<div class="portfolio-block">
+				<?php
+				if ( have_posts() ) :
+					
+					?>
+					<div class="portfolio-grid">
+					<?php
+
+						while ( have_posts() ) :
+
+							the_post();
+							
+							get_template_part( 'template-parts/content', 'portfolio' );
+
+						endwhile;
+
+					?>
+					</div>
+					<?php
+
+					the_posts_pagination( array(
+						'screen_reader_text' => ' ',
+						'next_text'          => '<img src="' . get_template_directory_uri() . '/img/pagination-arrow.png' . '">',
+						'prev_text'          => '',
+					) );
+
+				else :
+
+					get_template_part( 'template-parts/content', 'none' );
+
+				endif;
+				?>
+			</div>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_template_part( 'template-parts/content', 'portfolio-modal' );
+get_footer();
