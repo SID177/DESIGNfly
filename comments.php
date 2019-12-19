468c45e1361355b2adf1e55ff9d7d04ec82cf7d0
Siddharth Tikekar <siddharth.tikekar@rtcamp.com>
2019-12-19 15:03:39 +0530

    Remove unused code

--- comments.php
+++ comments.php
@@ -28,23 +28,6 @@ if ( post_password_required() ) {
 		?>
 		<span class="comments-title">
 			<?= esc_html_x( 'Comments', 'post comments', 'designfly' ) ?>
-			<?php
-			// $designfly_comment_count = get_comments_number();
-			// if ( '1' === $designfly_comment_count ) {
-			// 	printf(
-			// 		/* translators: 1: title. */
-			// 		esc_html__( 'One thought on &ldquo;%1$s&rdquo;', 'designfly' ),
-			// 		'<span>' . get_the_title() . '</span>'
-			// 	);
-			// } else {
-			// 	printf( // WPCS: XSS OK.
-			// 		/* translators: 1: comment count number, 2: title. */
-			// 		esc_html( _nx( '%1$s thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', $designfly_comment_count, 'comments title', 'designfly' ) ),
-			// 		number_format_i18n( $designfly_comment_count ),
-			// 		'<span>' . get_the_title() . '</span>'
-			// 	);
-			// }
-			?>
 		</span><!-- .comments-title -->
 
 		<?php the_comments_navigation(); ?>
