dcdb7278cb1d1a934c0772412fe1b69cf39972ad
Siddharth Tikekar <siddharth.tikekar@rtcamp.com>
2020-01-01 19:55:21 +0530

    Fix phpcs of functions.php file

--- functions.php
+++ functions.php
@@ -43,36 +43,49 @@ if ( ! function_exists( 'designfly_setup' ) ) :
 		add_theme_support( 'post-thumbnails' );
 
 		// This theme uses wp_nav_menu() in one location.
-		register_nav_menus( array(
-			'primary-menu' => esc_html__( 'Primary', 'designfly' ),
-		) );
+		register_nav_menus(
+			array(
+				'primary-menu' => esc_html__( 'Primary', 'designfly' ),
+			)
+		);
 
 		/*
 		 * Switch default core markup for search form, comment form, and comments
 		 * to output valid HTML5.
 		 */
-		add_theme_support( 'html5', array(
-			'search-form',
-			'comment-form',
-			'comment-list',
-			'gallery',
-			'caption',
-		) );
+		add_theme_support(
+			'html5',
+			array(
+				'search-form',
+				'comment-form',
+				'comment-list',
+				'gallery',
+				'caption',
+			)
+		);
 
 		// Set up the WordPress core custom background feature.
-		add_theme_support( 'custom-background', apply_filters( 'designfly_custom_background_args', array(
-			'default-color' => 'ffffff',
-			'default-image' => get_template_directory_uri() . '/img/rapeatable-bg.png',
-		) ) );
+		add_theme_support(
+			'custom-background',
+			apply_filters(
+				'designfly_custom_background_args',
+				array(
+					'default-color' => 'ffffff',
+					'default-image' => get_template_directory_uri() . '/img/rapeatable-bg.png',
+				)
+			)
+		);
 
 		// Add theme support for selective refresh for widgets.
 		add_theme_support( 'customize-selective-refresh-widgets' );
-		
+
 	}
 endif;
 add_action( 'after_setup_theme', 'designfly_setup' );
 
-
+/**
+ * Flush rewrite rules after switching themes to avoid page not found problems.
+ */
 function designfly_after_switch_theme() {
 	flush_rewrite_rules();
 }
@@ -131,11 +144,10 @@ function designfly_init() {
 		'public'      => true,
 		'menu_icon'   => 'dashicons-portfolio',
 		'has_archive' => true,
-		'supports'    => array( 'title', 'excerpt', 'thumbnail', 'page-attributes' )
+		'supports'    => array( 'title', 'excerpt', 'thumbnail', 'page-attributes' ),
 	);
 
 	register_post_type( 'designfly_portfolio', $args );
-
 
 	$labels = array(
 		'name'          => _x( 'Portfolio Categories', 'Portfolio category general name', 'designfly' ),
@@ -155,17 +167,18 @@ add_action( 'init', 'designfly_init' );
 
 /**
  * Replaces default read more text with READ MORE link.
- * 
+ *
  * @param string $more Default read more string.
- * 
+ *
  * @return string New read more string.
  */
 function designfly_excerpt_more( $more ) {
 	if ( ! is_single() ) {
-		$more = sprintf( '<a class="read-more" href="%1$s">%2$s</a>',
-            get_permalink( get_the_ID() ),
-            esc_html__( 'READ MORE', 'designfly' )
-        );
+		$more = sprintf(
+			'<a class="read-more" href="%1$s">%2$s</a>',
+			get_permalink( get_the_ID() ),
+			esc_html__( 'READ MORE', 'designfly' )
+		);
 	}
 
 	return $more;
@@ -174,9 +187,9 @@ add_filter( 'excerpt_more', 'designfly_excerpt_more' );
 
 /**
  * Replaces default read more text length.
- * 
+ *
  * @param int $length Default read more text length.
- * 
+ *
  * @return int New read more text length.
  */
 function designfly_excerpt_length( $length ) {
@@ -194,32 +207,32 @@ add_filter( 'excerpt_length', 'designfly_excerpt_length' );
  */
 function designfly_scripts() {
 
-	wp_enqueue_style( 'designfly-style', get_stylesheet_uri() );
+	wp_enqueue_style( 'designfly-style', get_stylesheet_uri(), array(), '1.0' );
 
 	wp_enqueue_style( 'dashicons' );
 
-	wp_enqueue_style( 'designfly-theme', get_template_directory_uri() . '/css/theme.css' );
-	wp_enqueue_script( 'designfly-theme', get_template_directory_uri() . '/js/theme.js', array( 'jquery' ), '20151215', true );
+	wp_enqueue_style( 'designfly-theme', get_template_directory_uri() . '/css/theme.css', array(), '1.0' );
+	wp_enqueue_script( 'designfly-theme', get_template_directory_uri() . '/js/theme.js', array( 'jquery' ), '1.0', true );
 
-	wp_enqueue_style( 'designfly-font', get_template_directory_uri() . '/css/open-sans.css' );
+	wp_enqueue_style( 'designfly-font', get_template_directory_uri() . '/css/open-sans.css', array(), '1.0' );
 
-	wp_enqueue_script( 'designfly-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+	wp_enqueue_script( 'designfly-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '1.0', true );
 
-	wp_enqueue_script( 'designfly-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
-	
+	wp_enqueue_script( 'designfly-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '1.0', true );
+
 	if ( is_front_page() || is_post_type_archive( 'designfly_portfolio' ) || is_tax( 'designfly_categories' ) ) {
-		
+
 		if ( is_front_page() ) {
-			wp_enqueue_style( 'designfly-front-page-style', get_template_directory_uri() . '/css/front-page.css' );
+			wp_enqueue_style( 'designfly-front-page-style', get_template_directory_uri() . '/css/front-page.css', array(), '1.0' );
 		}
 
-		wp_enqueue_style( 'designfly-portfolio', get_template_directory_uri() . '/css/portfolio.css' );
-		wp_enqueue_script( 'designfly-portfolio-modal', get_template_directory_uri() . '/js/portfolio-modal.js', array( 'designfly-theme' ), '20151215', true );
+		wp_enqueue_style( 'designfly-portfolio', get_template_directory_uri() . '/css/portfolio.css', array(), '1.0' );
+		wp_enqueue_script( 'designfly-portfolio-modal', get_template_directory_uri() . '/js/portfolio-modal.js', array( 'designfly-theme' ), '1.0', true );
 	}
 
 	if ( is_singular() && comments_open() ) {
 		global $post;
-		
+
 		if ( 'post' === $post->post_type ) {
 			$count = (int) get_post_meta( $post->ID, 'designfly_post_views_count', true );
 			if ( empty( $count ) ) {
@@ -230,7 +243,7 @@ function designfly_scripts() {
 			update_post_meta( $post->ID, 'designfly_post_views_count', $count );
 		}
 
-		wp_enqueue_script( 'font-awesome', get_template_directory_uri() . '/js/font-awesome-icons.js' );
+		wp_enqueue_script( 'font-awesome', get_template_directory_uri() . '/js/font-awesome-icons.js', array(), '1.0', true );
 
 		if ( get_option( 'thread_comments' ) ) {
 			wp_enqueue_script( 'comment-reply' );
@@ -242,14 +255,13 @@ add_action( 'wp_enqueue_scripts', 'designfly_scripts' );
 /**
  * Custom comment list item HTML for posts.
  * Used in wp_list_comments function.
- * 
+ *
  * @param object $comment Current comment object.
  * @param array  $args    Arguments passed in wp_list_comments function.
  * @param int    $depth   Depth of current comment children tree.
- * 
  */
 function designfly_custom_comment_block( $comment, $args, $depth ) {
-	$GLOBALS['comment'] = $comment;
+	$GLOBALS['comment'] = $comment; // phpcs:ignore
 
 	$tag = ( 'div' === $args['style'] ) ? 'div' : 'li';
 
@@ -261,12 +273,12 @@ function designfly_custom_comment_block( $comment, $args, $depth ) {
 	}
 
 	?>
-	<<?php echo $tag; ?> id="comment-<?php comment_ID(); ?>" <?php comment_class( '', $comment ); ?>>
+	<<?php echo $tag; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?> id="comment-<?php comment_ID(); ?>" <?php comment_class( '', $comment ); ?>>
 		<article id="div-comment-<?php comment_ID(); ?>" class="comment-body">
 			<span class="dashicons dashicons-testimonial"></span>
 			<div class="comment-block">
 				<footer class="comment-meta">
-					<?php 
+					<?php
 					$author_link = '';
 					if ( empty( $comment->comment_author_url ) && ! empty( $comment->user_id ) ) {
 						$author_link = '<a href="' . esc_url( get_author_posts_url( $comment->user_id ) ) . '">' . esc_html( get_the_author_meta( 'display_name', $comment->user_id ) ) . '</a>';
@@ -277,12 +289,12 @@ function designfly_custom_comment_block( $comment, $args, $depth ) {
 					$author_html  = '<span class="comment-author">' . $author_link . '</span>';
 					$said_on_html = ' <span class="said-on">' . esc_html_x( 'said on', 'designfly comment said on', 'designfly' ) . '</span> ';
 					$comment_date = get_comment_date( 'F d, Y', $comment ) . ' ' . esc_html_x( 'at', 'designfly comment at', 'designfly' ) . ' ' . get_comment_date( 'H:i a', $comment );
-					
-					echo $author_html . $said_on_html . $comment_date;
-					?>
-					
-					<?php if ( '0' == $comment->comment_approved ) : ?>
-					<em class="comment-awaiting-moderation"><?php echo $moderation_note; ?></em>
+
+					echo $author_html . $said_on_html . $comment_date; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+
+					if ( '0' == $comment->comment_approved ) : // phpcs:ignore
+						?>
+						<em class="comment-awaiting-moderation"><?php echo $moderation_note; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></em>
 					<?php endif; ?>
 				</footer><!-- .comment-meta -->
 
@@ -310,8 +322,11 @@ function designfly_custom_comment_block( $comment, $args, $depth ) {
 	<?php
 }
 
+/**
+ * Enqueue admin style.
+ */
 function designfly_admin_enqueue_scripts() {
-	wp_enqueue_style( 'designfly-admin', get_template_directory_uri() . '/css/admin.css' );
+	wp_enqueue_style( 'designfly-admin', get_template_directory_uri() . '/css/admin.css', array(), '1.0' );
 }
 add_action( 'admin_enqueue_scripts', 'designfly_admin_enqueue_scripts' );
 
@@ -341,15 +356,17 @@ require get_template_directory() . '/inc/widgets/class-designfly-twitter-widget.
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
 function designfly_widgets_init() {
-	register_sidebar( array(
-		'name'          => esc_html__( 'Sidebar', 'designfly' ),
-		'id'            => 'sidebar-1',
-		'description'   => esc_html__( 'Add widgets here.', 'designfly' ),
-		'before_widget' => '<section id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</section>',
-		'before_title'  => '<h2 class="widget-title">',
-		'after_title'   => '</h2>',
-	) );
+	register_sidebar(
+		array(
+			'name'          => esc_html__( 'Sidebar', 'designfly' ),
+			'id'            => 'sidebar-1',
+			'description'   => esc_html__( 'Add widgets here.', 'designfly' ),
+			'before_widget' => '<section id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</section>',
+			'before_title'  => '<h2 class="widget-title">',
+			'after_title'   => '</h2>',
+		)
+	);
 
 	register_widget( 'DESIGNfly_Portfolio_Widget' );
 	register_widget( 'DESIGNfly_Posts_Widget' );
