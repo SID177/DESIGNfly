808473000cb210e299bbc230b821cefaf3d4a05f
Siddharth Tikekar <siddharth.tikekar@rtcamp.com>
2020-01-05 17:05:55 +0530

    Add fallback image for portfolio features image

--- template-parts/content-portfolio.php
+++ template-parts/content-portfolio.php
@@ -9,7 +9,7 @@
 
 ?>
 <div <?php post_class( 'portfolio-item' ); ?> id="post-<?php the_ID(); ?>">
-	<img class="portfolio-img" src="<?php echo esc_url( get_the_post_thumbnail_url() ); ?>" alt="<?php echo esc_html( get_the_title() ); ?>">
+	<img class="portfolio-img" src="<?php echo has_post_thumbnail() ? esc_url( get_the_post_thumbnail_url() ) : esc_url( get_template_directory_uri() . '/img/noimagefound.png' ); ?>" alt="<?php echo esc_html( get_the_title() ); ?>">
 	<div class="portfolio-overlay">
 		<div class="portfolio-overlay-text">
 			<span class="dashicons dashicons-visibility portfolio-overlay-dashicon"></span><br/>
